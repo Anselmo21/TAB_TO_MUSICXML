aaafcae0fe0dc586b4f0635bd3bf534980cde12a
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-01-25 08:20:16 -0500

    fixed drag and drop so that only .txt and .log can be dropped into the GUI

--- TAB_TO_XML/src/main/java/TAB_TO_XML/GUI.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/GUI.java
@@ -1,12 +1,13 @@
 package TAB_TO_XML;
 
-
 import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -39,13 +40,13 @@ import javafx.stage.Stage;
 public class GUI extends Application {
 
 	private Desktop dt = Desktop.getDesktop(); //User Desktop 
-	
-	 final Button ob = new Button(); //Clickable button for opening the tab to search for the file to open
-	 final FileChooser fc = new FileChooser(); //
-	 final Label ol = new Label();
-	 final Label obl = new Label();
-	 final Label el = new Label();
-	 final ComboBox dropDownMenu = new ComboBox();
+
+	final Button ob = new Button(); //Clickable button for opening the tab to search for the file to open
+	final FileChooser fc = new FileChooser(); //
+	final Label ol = new Label();
+	final Label obl = new Label();
+	final Label el = new Label();
+	final ComboBox dropDownMenu = new ComboBox();
 
 	/**
 	 * Overridden start method that originally came from the Application class from Javafx. 
@@ -53,178 +54,205 @@ public class GUI extends Application {
 	 * @param primaryStage is the primary page of this application, onto which the application scene can be set.
 	 * <p> more stages can be created if the developers choose so!
 	 */
-    @Override
-    public void start(Stage primaryStage) {
-    	
-    	primaryStage.setTitle("TTT(TextToTablature)"); //Title
+	@Override
+	public void start(Stage primaryStage) {
 
-    	Button ob = new Button("Browse"); //Push button that is redirected to a file selecting page
-        
-    	FileChooser fc = new FileChooser(); //Opens browser to select the txt file
-        Label ol = new Label("TTT is a desktop application used for converting "
-        		+ "txt tablatures into formal tablatures."); //Description of the use of the desktop application
-        Label obl = new Label("Please select the tablature in txt format: "); //Indicator on what the button is for
-        Label el = new Label("");//Empty label
-        
-        //object that will do the "adding" of buttons in the GUI
-        final GridPane inputValues = new GridPane(); //The parent which concludes of children such as buttons and labels
-        final Pane rg = new VBox(12); //Base class that is used to have the children of inputValues public
-        
-        // DropDown menu construction
-       dropDownMenu.getItems().addAll("Guitar", "Drum", "Bass");
-       dropDownMenu.setValue("Instrument of Choice");
-       
-        
-        
-        
-        //Text Customization
-        Font font = Font.font("Browsed", FontWeight.EXTRA_BOLD, 40); //Font for the buttons
-        Font font1 = Font.font("label font", FontWeight.NORMAL, 20); //Font for the plan texts
+		primaryStage.setTitle("TTT(TextToTablature)"); //Title
 
-        ob.setFont(font);
-        obl.setFont(font1);
-        ol.setFont(font1);
-        
-        ob.setOnAction( //Sets the value of the button on Action.
+		Button ob = new Button("Browse"); //Push button that is redirected to a file selecting page
 
-        	new EventHandler<ActionEvent>() { //Interface for handling events
-        		
-        		
-        @Override
-        /**
-         * Invoked when 'browse' button is clicked...searching of files begins
-         * @param ActionEvent is the event associated when the browse button was clicked.
-         */
-        public void handle(final ActionEvent e) {
-        	
-        File fi = fc.showOpenDialog(primaryStage); //Pops up an "Open File" file chooser dialog
-        if (fi != null) {
-        	
-        	openFile(fi); //opens the selected file 
-        	
-        	}
-        
-        else { // display error message if file doesn't exist
-        	
-        	final Label notification = new Label();
-        	notification.setText("You have not selected a textFile");
-        	
-        		}
-        
-        	}
-        
-        }); 
-        
-        // Drag & Drop feature 
-        Label label = new Label("Drag And Drop A File Here");
-        Label dropped = new Label("");
-        VBox dragTarget = new VBox();
-        dragTarget.getChildren().addAll(label,dropped);
-        dragTarget.setOnDragOver(new EventHandler<DragEvent>() {
+		FileChooser fc = new FileChooser(); //Opens browser to select the txt file
+		Label ol = new Label("TTT is a desktop application used for converting "
+				+ "txt tablatures into formal tablatures."); //Description of the use of the desktop application
+		Label obl = new Label("Please select the tablature in txt format: "); //Indicator on what the button is for
+		Label el = new Label("");//Empty label
 
-            @Override
-            public void handle(DragEvent event) {
-                if (event.getGestureSource() != dragTarget
-                        && event.getDragboard().hasFiles()) { 
-                    
-                	/* allow for both copying and moving, whatever user chooses */
-                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                }
-                event.consume(); // you don't want the event to be dispatched to any further event listeners.
-            //events are dispatched in last in, first out fashion
-            }
-        });
-        
-        dragTarget.setOnDragDropped(new EventHandler<DragEvent>() {
+		//object that will do the "adding" of buttons in the GUI
+		final GridPane inputValues = new GridPane(); //The parent which concludes of children such as buttons and labels
+		final Pane rg = new VBox(12); //Base class that is used to have the children of inputValues public
 
-            @Override
-            public void handle(DragEvent event) {
-                Dragboard db = event.getDragboard(); //assign event's dragboard to a variable for ease 
-                boolean success = false;
-                if (db.hasFiles()) {
-                    dropped.setText(db.getFiles().toString()); //The text to display in the label. 
-                    success = true;
-                   
-                }
-                openFile(db.getFiles().get(0));
-                /* let the source know whether the string was successfully 
-                 * transferred and used */
-                event.setDropCompleted(success);
-
-                event.consume(); 
-            }
-        });
-        
-        StackPane root = new StackPane();
-        inputValues.add(dragTarget, 1, 6);
-        // For some reason it's not letting me drop anything stil....lol
-
-       
+		// DropDown menu construction
+		dropDownMenu.getItems().addAll("Guitar", "Drum", "Bass");
+		dropDownMenu.setValue("Instrument of Choice");
 
 
-        
-        
-        		
-        //Restricts the allowable columns and rows for the location of each text or button
 
-        inputValues.add(dropDownMenu, 2, 5);  //adds the drop-down menu 
-        inputValues.add(ob, 1, 2);
-        inputValues.add(obl, 0, 2);
-        inputValues.add(el, 0, 1);
-        inputValues.add(ol, 0, 0);
-        inputValues.setHgap(6); //The width of the horizontal gaps between columns.
-        inputValues.setVgap(6); //The length of the vertical gaps between rows       
-        
-        rg.getChildren().addAll(inputValues); //method from superclass 'Pane', this gets all the children and places them in the GUI
-        rg.setPadding(new Insets(12, 12, 12, 12));
-        
-        //create a scene for the primary page
-        Scene Scene1 = new Scene(rg); 
-        primaryStage.setScene(Scene1); 
-        primaryStage.show(); //make the primary page visible to the user
-    }
-    
-    /**
+
+		//Text Customization
+		Font font = Font.font("Browsed", FontWeight.EXTRA_BOLD, 40); //Font for the buttons
+		Font font1 = Font.font("label font", FontWeight.NORMAL, 20); //Font for the plan texts
+
+		ob.setFont(font);
+		obl.setFont(font1);
+		ol.setFont(font1);
+
+		ob.setOnAction( //Sets the value of the button on Action.
+
+				new EventHandler<ActionEvent>() { //Interface for handling events
+
+
+					@Override
+					/**
+					 * Invoked when 'browse' button is clicked...searching of files begins
+					 * @param ActionEvent is the event associated when the browse button was clicked.
+					 */
+					public void handle(final ActionEvent e) {
+
+						File fi = fc.showOpenDialog(primaryStage); //Pops up an "Open File" file chooser dialog
+						if (fi != null) {
+
+							openFile(fi); //opens the selected file 
+
+						}
+
+						else { // display error message if file doesn't exist
+
+							final Label notification = new Label();
+							notification.setText("You have not selected a textFile");
+
+						}
+
+					}
+
+				}); 
+
+		// Drag & Drop feature 
+		Label label = new Label("Drag And Drop A File Here");
+		Label dropped = new Label("");
+
+		try {
+			VBox dragTarget = new VBox();
+
+			//Extensions that are valid to be dragged and dropped
+			List<String> validExtensions = Arrays.asList("txt", "log");
+
+			dragTarget.getChildren().addAll(label,dropped);
+			dragTarget.setOnDragOver(new EventHandler<DragEvent>() {
+
+				@Override
+				public void handle(DragEvent event) {
+					if (event.getGestureSource() != dragTarget
+							&& event.getDragboard().hasFiles()) { 
+
+						//All files must have the accepted extension
+						if (!validExtensions.containsAll(event.getDragboard().getFiles().stream().map(file -> getExtension(file.getName())).collect(Collectors.toList()))) {
+							event.consume();
+							return;
+						}
+
+						/* allow for both copying and moving, whatever user chooses */
+						event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+					}
+					event.consume(); // you don't want the event to be dispatched to any further event listeners.
+					//events are dispatched in last in, first out fashion
+				}
+			});
+
+			dragTarget.setOnDragDropped(new EventHandler<DragEvent>() {
+
+				@Override
+				public void handle(DragEvent event) {
+					Dragboard db = event.getDragboard(); //assign event's dragboard to a variable for ease 
+					boolean success = false;
+					if (event.getGestureSource() != dragTarget && db.hasFiles()) {
+						dropped.setText(db.getFiles().toString()); //The text to display in the label. 
+						event.getDragboard().getFiles().forEach(file -> System.out.println(file.getAbsolutePath()));
+						success = true;
+
+					}
+					openFile(db.getFiles().get(0));
+					/* let the source know whether the string was successfully 
+					 * transferred and used */
+					event.setDropCompleted(success);
+
+					event.consume(); 
+				}
+			});
+
+			StackPane root = new StackPane();
+			inputValues.add(dragTarget, 1, 6);
+			// For some reason it's not letting me drop anything stil....lol
+
+
+
+
+
+
+
+			//Restricts the allowable columns and rows for the location of each text or button
+
+			inputValues.add(dropDownMenu, 2, 5);  //adds the drop-down menu 
+			inputValues.add(ob, 1, 2);
+			inputValues.add(obl, 0, 2);
+			inputValues.add(el, 0, 1);
+			inputValues.add(ol, 0, 0);
+			inputValues.setHgap(6); //The width of the horizontal gaps between columns.
+			inputValues.setVgap(6); //The length of the vertical gaps between rows       
+
+			rg.getChildren().addAll(inputValues); //method from superclass 'Pane', this gets all the children and places them in the GUI
+			rg.setPadding(new Insets(12, 12, 12, 12));
+
+			//create a scene for the primary page
+			Scene Scene1 = new Scene(rg); 
+			primaryStage.setScene(Scene1); 
+			primaryStage.show(); //make the primary page visible to the user
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//Getter method to get the extension of the file.
+	private String getExtension (String fileName) {
+		String extension = "";
+		int i = fileName.lastIndexOf('.');
+		if (i > 0 && i < fileName.length() - 1) //if the name is not empty
+			return fileName.substring(i + 1).toLowerCase();
+		return extension;
+	}
+
+	/**
 	 * The method that opens fc and takes a input file. Throws an IOException when the file is corrupted.
 	 * @param fi is the file to be opened
 	 */
-    private void openFile(File fi) {
-    	
-    	//This opens the file of your choice, but it's not necessary at the moment as it's only for testing whether this method works or not
-    	/**
+	private void openFile(File fi) {
+
+		//This opens the file of your choice, but it's not necessary at the moment as it's only for testing whether this method works or not
+		/**
     	try {
             dt.open(fi); 
         } catch (IOException ex) {
             Logger.getLogger(GUI.class.getName())
             .log(Level.SEVERE, null, ex);
         } 
-    	*/ 
-    	
-    	try (BufferedReader reader = new BufferedReader(new FileReader(fi))) { //Reads the file that was selected by the user
+		 */ 
 
-            ArrayList<Character> datain = new ArrayList<Character>(); //Arraylist that stores the data from the txt file character by character
-            int i = 0;
-            int j = 0;
-            while ((i = reader.read()) != -1) {
-            	char ch = (char) i;
-            	datain.add(ch);
-            	/**
+		try (BufferedReader reader = new BufferedReader(new FileReader(fi))) { //Reads the file that was selected by the user
+
+			ArrayList<Character> datain = new ArrayList<Character>(); //Arraylist that stores the data from the txt file character by character
+			int i = 0;
+			int j = 0;
+			while ((i = reader.read()) != -1) {
+				char ch = (char) i;
+				datain.add(ch);
+				/**
             	System.out.println(datain.get(j));
             	j++;
-            	*/
-            }
+				 */
+			}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    	
-    }
-    
-    /**
-     * Main function that runs the Application.launch() method
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Main function that runs the Application.launch() method
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
