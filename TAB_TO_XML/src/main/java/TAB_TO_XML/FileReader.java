9efab63949dcdb0b11420ad601df399ea439bdf1
Rafael Dolores <rafd47@my.yorku.ca>
2021-01-26 10:02:19 -0500

    Attempt No.2

--- TAB_TO_XML/src/main/java/TAB_TO_XML/FileReader.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/FileReader.java
@@ -9,19 +9,12 @@ public class FileReader {
 	
 	public static void main(String[] args) throws FileNotFoundException { 
 		
-		Scanner first_scan = new Scanner(System.in);
-		System.out.println("Enter a file to be scanned: ");
-		String file = first_scan.nextLine();
-		File scannedFile = new File(file);
-		Scanner second_scan = new Scanner(scannedFile);
-		
-		String fileContent = ""; 
-		while(second_scan.hasNextLine()) { 
-			fileContent = fileContent.concat(second_scan.nextLine() + "\n");	
+		File file = new File("\"C:\\Users\\rafae\\OneDrive\\Desktop\\Map.java\"");
+		Scanner scan = new Scanner(file);
+		while(scan.hasNextLine()) { 
+			System.out.println(scan.hasNextLine());
 		}
-		System.out.println(fileContent);
-		first_scan.close();
-		second_scan.close();
+		scan.close();
 	}
 
 	
