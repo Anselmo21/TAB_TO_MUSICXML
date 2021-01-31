519f80498a9e0c421f11427456d99a8fabcaf704
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-01-31 17:56:57 -0500

    Wrote some XML document building and transformer stuff

--- TAB_TO_XML/src/main/java/TAB_TO_XML/Drums.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/Drums.java
@@ -1,5 +1,43 @@
 package TAB_TO_XML;
 
+import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class Drums {
+
+	public static void convertToXML (ArrayList<Character> components) {
+
+		try {
+			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+			Document document = documentBuilder.newDocument();
+
+			TransformerFactory trff= TransformerFactory.newInstance(); 
+			Transformer trf = trff.newTransformer(); 
+			DOMSource dom= new DOMSource(document);
+		}
+
+		catch (ParserConfigurationException p) {
+			p.printStackTrace();
+		}	
+		catch (TransformerException t) {
+			t.printStackTrace();
+		}
+	}
 
 }
