6c5e30cb066e4c39e1c62c883a1753cf34f9ac2b
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-01-31 18:27:00 -0500

    Added some comments to the drum class

--- TAB_TO_XML/src/main/java/TAB_TO_XML/Drums.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/Drums.java
@@ -23,14 +23,27 @@ public class Drums {
 	public static void convertToXML (ArrayList<Character> components) {
 
 		try {
-			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+			//This creates a Dom object trees that will store elements of an XML documents
+			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance(); 
+			//an instance of class obtained from the DocumentBuilderFactory.newDocumentBuilder() method. XML can be parsed from a variety of input sources
+			//This method of writing XML works like a tree, there are top elements that are parents of child elements.
 			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+			//root element (top parent)
 			Document document = documentBuilder.newDocument();
 
-			TransformerFactory trff= TransformerFactory.newInstance(); 
-			Transformer trf = trff.newTransformer(); 
-			DOMSource dom= new DOMSource(document);
-			StreamResult streamresult = new StreamResult(new File("../TAB_TO_XML/src/main/java/"));
+			//Used to create Transformer objects.
+			TransformerFactory trff= TransformerFactory.newInstance();
+			//A class that can transform a source tree into a result tree.
+			Transformer trf = trff.newTransformer();   
+			//Document Object Model (DOM) tree that acts as a holder for a transformation source tree.
+			DOMSource dom= new DOMSource(document);    
+			//This creates a XML file that is being translated from the DOMSource
+			StreamResult streamresult = new StreamResult(new File("../TAB_TO_XML/src/main/java/"));    
+			
+			//This translates your XML source to a result (in this case, everything under document to StreamResult)
+			trf.transform(dom, streamresult);   
+			trf.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "-//Recordare//DTD MusicXML 3.1 Partwise//EN");
+			trf.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "http://www.musicxml.org/dtds/partwise.dtd");
 		}
 
 		catch (ParserConfigurationException p) {
