ea89c5e2ed1e80231f8f3a8a415f6f04fb6e6a6e
Shawn Verma <shawn.verma3@gmail.com>
2021-02-01 00:58:26 -0500

    Added cowbell class with skeleton for xml

---
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/Cowbell.java
@@ -0,0 +1,88 @@
+package TAB_TO_XML;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Cowbell {
+
+	public static void convertToXML (ArrayList<Character> components) {
+
+		try {
+			
+			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance(); 
+			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+			Document document = documentBuilder.newDocument();
+			
+			Element scorePW = document.createElement("score-partwise");
+			Attr scorePW_att = document.createAttribute("version");
+			scorePW_att.setValue("3.1");
+			scorePW.setAttributeNode(scorePW_att);
+			document.appendChild(scorePW);
+			
+			Element partList = document.createElement("part-list");
+			scorePW.appendChild(partList);
+
+			Element scorePart = document.createElement("part-list");
+			Attr scorePart_att = document.createAttribute("id");
+			scorePart_att.setValue("P1");
+			scorePW.setAttributeNode(scorePart_att);
+			partList.appendChild(scorePart);
+			
+			Element partName = document.createElement("part-name");
+			partName.appendChild(document.createTextNode("Music"));
+			scorePart.appendChild(partName);
+			
+			Element part = document.createElement("part");
+			Attr part_att = document.createAttribute("id");
+			part_att.setValue("P1");
+			part.setAttributeNode(part_att);
+			scorePW.appendChild(part);
+			
+			Element measure = document.createElement("measure");
+			Attr measure_att = document.createAttribute("number");
+			measure_att.setValue("1");
+			measure.setAttributeNode(measure_att);
+			part.appendChild(measure);
+			
+			Element attributes = document.createElement("attributes");
+			measure.appendChild(attributes);
+
+
+			//Used to create Transformer objects.
+			TransformerFactory trff= TransformerFactory.newInstance();
+			//A class that can transform a source tree into a result tree.
+			Transformer trf = trff.newTransformer();   
+			//Document Object Model (DOM) tree that acts as a holder for a transformation source tree.
+			DOMSource dom= new DOMSource(document);    
+			//This creates a XML file that is being translated from the DOMSource
+			StreamResult streamresult = new StreamResult(new File("../TAB_TO_XML/src/main/java/"));    
+			
+			//This translates your XML source to a result (in this case, everything under document to StreamResult)
+			trf.transform(dom, streamresult);   
+			trf.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "-//Recordare//DTD MusicXML 3.1 Partwise//EN");
+			trf.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "http://www.musicxml.org/dtds/partwise.dtd");
+		}
+
+		catch (ParserConfigurationException p) {
+			p.printStackTrace();
+		}	
+		catch (TransformerException t) {
+			t.printStackTrace();
+		}
+	}
+
+}
