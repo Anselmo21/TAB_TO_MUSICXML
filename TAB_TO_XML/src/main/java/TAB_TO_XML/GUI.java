9bb3809fda154d7f73323b01d9659821181f6f79
Rafael Dolores <rafd47@my.yorku.ca>
2021-02-08 11:53:34 -0500

    fixed

--- TAB_TO_XML/src/main/java/TAB_TO_XML/GUI.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/GUI.java
@@ -163,7 +163,7 @@ public class GUI extends Application {
 							inputValues.add(ob2,2,2);
 							ob2.setOnAction(new EventHandler<ActionEvent>() { //if you feed a proper file..convert button will appear 
 								public void handle(ActionEvent a) {
-								openFile(fi);
+								convertToXML(fi);
 								Alert errorAlert = new Alert(AlertType.CONFIRMATION); //creates a displayable error allert window 
 								errorAlert.setHeaderText("The selected file is being converted to XML"); 
 								errorAlert.setContentText("The process might take a while..."); //Shows this stage and waits for it to be hidden (closed) before returning to the caller.
