66c767bde27a26f6f8e16f564daa06d6be1d3839
Rafael Dolores <rafd47@my.yorku.ca>
2021-02-19 10:14:34 -0500

    Made new classes for DRUMS

--- TAB_TO_XML/src/main/java/Model/Beam.java
+++ TAB_TO_XML/src/main/java/Model/Beam.java
@@ -1,5 +1,24 @@
 package Model;
 
-public class Beam {
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+/**
+ * This class is meant for the drums only
+ *
+ */
+public class Beam {
+	
+	@JacksonXmlProperty(isAttribute=true)
+	int number; 
+	
+	public void setNumber(int num) { 
+		this.number = num;
+		
+	}
+	
+	public int getNumber() {
+		
+		return this.number;
+		
+	}
 }
--- TAB_TO_XML/src/main/java/Model/ScoreInstrument.java
+++ TAB_TO_XML/src/main/java/Model/ScoreInstrument.java
@@ -1,5 +1,49 @@
 package Model;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+/**
+ * THIS CLASS IS MEANT FOR THE DRUMS ONLY
+ * DO NOT IMPLEMENT FOR THE GUITAR
+ *
+ */
 public class ScoreInstrument {
 
+	
+	@JacksonXmlProperty(isAttribute=true)
+	String id;
+	
+	@JacksonXmlProperty(localName = "instrument-name")
+	String instrumentName;
+	
+	public ScoreInstrument(String id, String name) { 
+		
+		this.id = id;
+		this.instrumentName = name;
+		
+	}
+	
+	public String getInstrumentName() {
+		
+		return this.instrumentName;
+		
+	}
+	
+	public void setInstrumentName(String name) { 
+		
+		this.instrumentName = name;
+		
+	}
+	
+	public String getID() {
+		
+		return this.id;
+		
+	}
+	
+	public void setID(String id) { 
+		
+		this.id = id;
+		
+	}
 }
