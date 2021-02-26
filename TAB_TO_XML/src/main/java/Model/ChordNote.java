4e5715e8ed9448219f1c67040cf189a82c910e69
Shawn Verma <shawn.verma3@gmail.com>
2021-02-25 20:43:20 -0500

    fixed formatting

--- TAB_TO_XML/src/main/java/Model/ChordNote.java
+++ TAB_TO_XML/src/main/java/Model/ChordNote.java
@@ -3,10 +3,11 @@ package Model;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 public class ChordNote extends Note {
-	
+
 	@JacksonXmlProperty(localName = "chord")
 	String chord;
-	 public void setChord() { 
-		   this.chord = null;
-	   }
+
+	public void setChord() {
+		this.chord = null;
+	}
 }
