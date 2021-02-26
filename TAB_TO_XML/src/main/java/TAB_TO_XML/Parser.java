8fc90b8e2d17267ee22bfdd14e5f71b7628e585f
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-02-25 21:49:56 -0500

    Committing these files so i can switch to develop. no changes but still pushing

--- TAB_TO_XML/src/main/java/TAB_TO_XML/Parser.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/Parser.java
@@ -83,126 +83,12 @@ public class Parser {
 	}
 
 	/**
-	 * Store 6 lines of string (line of notes) from the very first line of notes
-	 * from the root.
-	 * 
-	 * @param root is the array list of strings that contains the strings from the
-	 *             file.
-	 * @return array parse which stores the very first line of notes that is within
-	 *         the root.
-	 */
-	public static ArrayList<String> extractStrings(ArrayList<String> root) {
-		ArrayList<String> parse = new ArrayList<String>();
-		for (int i = 0; i < root.size(); i++) {
-			if (root.get(i).contains("|-")) {
-				for (int j = i; j < i + 6; j++) {
-					parse.add(root.get(j));
-				}
-				break;
-			}
-		}
-		return parse;
-	}
-
-	/**
-	 * Remove 6 lines of string (a line of notes) from the root array list.
-	 * 
-	 * @param root is the array list of strings that contains the strings from the
-	 *             file.
-	 * @return root array after removing the very first line of notes that is within
-	 *         the root.
-	 */
-	public static ArrayList<String> reduceRoot(ArrayList<String> root) {
-		for (int i = 0; i < root.size(); i++) {
-			if (root.get(i).contains("|-")) {
-				for (int j = i; j < i + 6; j++) {
-					root.remove(i);
-				}
-				break;
-			}
-		}
-		return root;
-	}
-	
-	/**
-	 * Find the x coordinate of the next note to print
-	 * 
-	 * @param parse is the array list of strings that contains a whole line of notes, notenum is the number of the next note to print.
-	 * @return an integer representing the x position of string where the very next
-	 *         note is
-	 */
-	public static int findnotexposition(ArrayList<String> newParse, int notenum) {
-		int position = 0;
-		int a = 0;
-		outerloop: 
-			for (int i = 0; i < newParse.get(0).length() - 1; i++) {
-				for (int j = 0; j < newParse.size(); j++) {
-					if (Character.isDigit(newParse.get(j).charAt(i))) {
-						if (a == notenum) {
-							position = i;
-							break outerloop;
-						}	else {
-							a++;
-						}
-					} 
-				}
-				} 
-		return position;
-	}
-	
-	/**
-	 * Find the y coordinate of the next note to print
-	 * 
-	 * @param parse is the array list of strings that contains a whole line of notes, notenum is the number of the next note to print.
-	 * @return an integer representing the y position of string where the very next
-	 *         note is
-	 */
-	public static int findnoteyposition(ArrayList<String> newParse, int notenum) {
-		int position = 0;
-		int a = 0;
-		outerloop: 
-			for (int i = 0; i < newParse.get(0).length() - 1; i++) {
-				for (int j = 0; j < newParse.size(); j++) {
-					if (Character.isDigit(newParse.get(j).charAt(i))) {
-						if (a == notenum) {
-							position = j;
-							break outerloop;
-						}	else {
-							a++;
-						}
-					} 
-				}
-				} 
-		return position;
-	}
-
-	/**
-	 * Find the longest string that's closest to the top The returned string is
-	 * reduced by 1 and must be added by 1 for printing purposes.
-	 * 
-	 * @param parse is the array list of strings that contains a whole line of notes
-	 * @return an integer representing the position of string where the very next
-	 *         note is
-	 */
-	public static int findLongerList(ArrayList<String> parse) {
-		int count = parse.get(0).length();
-		int position = 0;
-		for (int i = 0; i < parse.size(); i++) {
-			if (count < parse.get(i).length()) {
-				position = i;
-				break;
-			}
-		}
-		return position;
-	}
-
-	/**
 	 * Counts duration of a note.
 	 * 
 	 * @param parse is the array list of strings that contains a whole line of notes
 	 * @return an integer representing the duration of the note.
 	 */
-	public static String durationCount(ArrayList<String> parse, int row, int column) {
+	public static Integer durationCount(ArrayList<String> parse, int row, int division) {
 		Integer count = 1;
 		
 		outerloop: 
@@ -218,55 +104,11 @@ public class Parser {
 					}
 				}
 			}
-		count = count / 2;
+		count = count / division;
 		if (count > 8) {
 			count = 8;
 		}
-		return count.toString();
-	}
-
-	/**
-	 * Helper method on reducing the strings in the array correctly every time you
-	 * print out a note
-	 * 
-	 * @param newParse is the array list of strings that contains a whole line of
-	 *                 notes
-	 * @return an Array list parse that's been reduced
-	 */
-	public static ArrayList<String> listReduction(ArrayList<String> parse) {
-
-		ArrayList<String> newParse = new ArrayList<>();
-
-		// deep copy
-		for (String s : parse) {
-			newParse.add(s);
-		}
-
-		outerloop: for (int i = 0; i < newParse.get(findLongerList(newParse)).length() - 1; i++) {
-			if (i == 0) {
-				int a = findLongerList(newParse);
-				if (Character.isDigit(newParse.get(a).charAt(0))) {
-					newParse.set(a, newParse.get(a).substring(1));
-					a = a + 1;
-				}
-				for (int j = a; j < newParse.size(); j++) {
-					if (Character.isDigit(newParse.get(j).charAt(0))) {
-						break outerloop;
-					} else {
-						newParse.set(j, newParse.get(j).substring(1));
-					}
-				}
-			} else {
-				for (int j = 0; j < newParse.size(); j++) {
-					if (Character.isDigit(newParse.get(j).charAt(0))) {
-						break outerloop;
-					} else {
-						newParse.set(j, newParse.get(j).substring(1));
-					}
-				}
-			}
-		}
-		return newParse;
+		return count;
 	}
 
 	/// May require fix again
@@ -279,28 +121,13 @@ public class Parser {
 	public static int divisionCount(ArrayList<String> parse) {
 		int k = 0;
 		// loop used to count the divisions of the tablature
-		for (int i = 1; i < parse.get(findLongerList(parse)).length(); i++) {
-			if (parse.get(0).charAt(i) == '|') {
+		for (int i = 1; i < parse.get(0).length()-1; i++) {
 				k++;
 			}
-		}
-		k = k - 2;
+		k = k / 4;
 		return k;
 	}
 
-	/**
-	 * Counts the fret of the note.
-	 * 
-	 * @param parse is the array list of strings that contains a whole line of notes
-	 * @return an integer that represents the fret of the note.
-	 */
-	public static String fretCount(ArrayList<String> parse) {
-		// System.out.println(findLongerList(parse));
-		Integer a = Character.getNumericValue(parse.get(findLongerList(parse)).charAt(0));
-		String fret = a.toString();
-		// System.out.println(fret);
-		return fret;
-	}
 
 	/**
 	 * Method used to get the step count.
@@ -308,7 +135,6 @@ public class Parser {
 	 * @param parse is the array list of strings that contains a whole line of notes
 	 * @return an String that represents the fret of the note.
 	 */
-
 	public static String stepCount(int row, int column) {
 
 		String[][] fretboard = new String[][] {
@@ -358,14 +184,12 @@ public class Parser {
 				for (int j = 0; j < 6; j++) {
 					eachCollection.add(inputFile.get(i+j));
 				}
-			}
-			
-			// checks to see if reached six lines
-			
-			if((i+1)%6==0) {							
+				eachCollection.add(" ");
 				Collections.add(eachCollection);
-				eachCollection = new ArrayList<String>();	
+				eachCollection = new ArrayList<String>();
+				i = i + 5;
 			}
+			
 			
 		}
 		
@@ -394,17 +218,6 @@ public class Parser {
 											
 	}
 	
-	public static int countNote(ArrayList<String> parse) {
-		int numNotes = 0;
-		for (int i = 0; i < parse.size(); i++) {
-			for (int j = 0; j < parse.get(0).length(); j++) {
-				if (Character.isDigit(parse.get(i).charAt(j))) {
-					numNotes++;
-				}
-			}
-		}
-		return numNotes;
-	}
 	public String parseAlter(String note) { 
 		
 		for (int i = 0; i < note.length(); i++) { 
