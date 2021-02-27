9f1f5dc95d73e95861b15a8c6ec022827200a0de
Shawn Verma <shawn.verma3@gmail.com>
2021-02-26 22:03:31 -0500

    remove score instrument from model package

--- TAB_TO_XML/src/main/java/Model/ScorePart.java
+++ TAB_TO_XML/src/main/java/Model/ScorePart.java
@@ -8,29 +8,10 @@ public class ScorePart {
 
 	@JacksonXmlProperty(localName = "part-name")
 	String partName; 
-	
-	//@JacksonXmlProperty(localName = "score-instrument")
-	//ScoreInstrument[] instruments; //Only for the drums!
-	//Don't include in guitar implementation
 
 	public ScorePart() {}
 	
-	/**
-	 * <p> This a constructor specifically for drums </p>
-	 * @param id
-	 * @param partName
-	 * @param instruments
-	 */
-	public ScorePart(String id, String partName, ScoreInstrument[] instruments) { 
-		
-		super(); 
-
-		this.id = id; 
-		this.partName = partName; 
-		//this.instruments = instruments;
-		
-		
-	}
+	
 	public ScorePart(String id, String partName) {
 		super();
 		this.id = id;
