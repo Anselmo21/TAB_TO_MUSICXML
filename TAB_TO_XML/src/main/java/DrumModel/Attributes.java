abed21b4e08f56f085696a7a73d8b8c1c4a70b3b
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-02-28 07:10:46 -0500

    Commented out imports to remove some errors in drum model for app.java

--- TAB_TO_XML/src/main/java/DrumModel/Attributes.java
+++ TAB_TO_XML/src/main/java/DrumModel/Attributes.java
@@ -2,10 +2,10 @@ package DrumModel;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
-import guitarModel.Clef;
-import guitarModel.Key;
+//import guitarModel.Clef;
+//import guitarModel.Key;
 import guitarModel.StaffDetails;
-import guitarModel.Time;
+//import guitarModel.Time;
 
 public class Attributes {
 	int divisions;
