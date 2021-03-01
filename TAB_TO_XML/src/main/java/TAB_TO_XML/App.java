577fa9ebf5d1ea0dedf14b24a45916f2670c462d
Shawn Verma <shawn.verma3@gmail.com>
2021-02-28 22:35:00 -0500

    fixed helper method for instrument identification

--- TAB_TO_XML/src/main/java/TAB_TO_XML/App.java
+++ TAB_TO_XML/src/main/java/TAB_TO_XML/App.java
@@ -657,14 +657,12 @@ public class App {
 	 * @return
 	 */
 	private static int helpMe(ArrayList<String> content) {
-		ArrayList<ArrayList<String>> getInstrument = new ArrayList<>();
-		ArrayList<String> storeInstrument = new ArrayList<>();
+		int count = 0;
 		for (int i = 0; i < content.size(); i++) {
-			if (content.get(i) != "") storeInstrument.add(content.get(i));
+			if (!content.get(i).equals("")) count++;
 			else break;
 		}
-		getInstrument.add(storeInstrument);
-		return getInstrument.get(0).size();
+		return count;
 	}
 
 }
