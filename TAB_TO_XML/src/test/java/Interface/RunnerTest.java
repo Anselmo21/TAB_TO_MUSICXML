45a1c6f2cfe45d0fd2570619f583edba20682c2c
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2021-03-15 22:12:30 -0400

    Fixed error in runnertest.java

--- TAB_TO_XML/src/test/java/Interface/RunnerTest.java
+++ TAB_TO_XML/src/test/java/Interface/RunnerTest.java
@@ -1,5 +1,6 @@
-package guiTest;
+package Interface;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
@@ -10,38 +11,36 @@ import org.testfx.matcher.control.LabeledMatchers;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 @ExtendWith(ApplicationExtension.class)
 public class RunnerTest {
 	
 	@Start
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Interface/Intro.fxml"));
-			Scene scene = new Scene(root, 935, 700);
-			scene.getStylesheets().add(getClass().getClassLoader().getResource("Interface/Intro.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Interface/Intro.fxml"));
+		primaryStage.setTitle("Allegro Tab Converter");
+		Scene scene = new Scene(root, 935, 700);
+		scene.getStylesheets().add(getClass().getClassLoader().getResource("Interface/Intro.css").toExternalForm());
+		Image logo = new Image("/Interface/music-background.jpg");
+		primaryStage.getIcons().add(logo);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
+	@Test
 	void browseButton(FxRobot robot) {
 		robot.clickOn("#browse");
-		FxAssert.verifyThat("#browse", LabeledMatchers.hasText("Browse"));
+		FxAssert.verifyThat("#browse", LabeledMatchers.hasText("Browse File"));
 	}
 	
-//	void browseFileChooserOpened(FxRobot robot) {
-//		robot.clickOn("#browse");
-//	}
-	
+	@Test
 	void convertButton(FxRobot robot) {
 		FxAssert.verifyThat("#convert", LabeledMatchers.hasText("Convert"));
 	}
 	
+	@Test
 	void saveButton(FxRobot robot) {
 		FxAssert.verifyThat("#save", LabeledMatchers.hasText("Save File"));
 	}
